f83ca063067647148e6d1f8c384b27f4fcbe8285
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-02-23 18:29:32 +0530

    data and types lec 010

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -1,5 +1,22 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 
-40 + 8 + 23 - 10;
+// 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23)
+let firstName = "Jonas";
+console.log(firstName)
+
+let Jonas_matilda = "JM";
+let $function = 8; // start reserve keyword with _ or $ for variable name use
+let PI = 3.1415; // constants variable name is written with al capital letters
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher"; // better style
+
+let job1 = "Programmer";
+let job2 = "Teacher"; // good style
+
+console.log(myFirstJob, myCurrentJob);

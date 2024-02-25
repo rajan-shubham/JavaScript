d56154a5d75805636d55deff9a11b0066e54fc9a
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-02-25 11:56:18 +0530

    Data & Types decleratiation initiation & print

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/*let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN!");
 
 // 40 + 8 + 23 - 10;
@@ -19,4 +19,29 @@ let myCurrentJob = "Teacher"; // better style
 let job1 = "Programmer";
 let job2 = "Teacher"; // good style
 
-console.log(myFirstJob, myCurrentJob);
+console.log(myFirstJob, myCurrentJob);*/
+/*
+// <lec->012>, Data & Types
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+//console.log(typeof true)
+console.log(typeof javaScriptIsFun);
+//console.log(typeof 23);
+//console.log('jonas');
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(null)
+console.log(typeof "anurag")
+*/
+

ad941bacafa365aba786a03e989585d5d6b3b147
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-02-26 10:57:55 +0530

    Let const & var

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -44,4 +44,19 @@ console.log(typeof year);
 console.log(null)
 console.log(typeof "anurag")
 */
+/*
+// <lec->013>, let,const and var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = "Rajan";
+console.log(lastName);*/
 

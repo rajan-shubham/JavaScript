24e801a1cac39a81b089c6c9984da633eb41db64
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-02-28 18:10:00 +0530

    Operator precedence

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -90,4 +90,19 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018); // line 87 & 92 is same*/
+/*
+//<LEC->015> {Operator Precedence}
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);*/
 

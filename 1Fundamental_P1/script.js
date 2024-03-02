2201479bbe6c0ccf7779b09443dc64053547959c
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-03-02 14:23:40 +0530

    basic IF & ELSE statement

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -167,3 +167,26 @@ console.log(`String
 with
 multiple
 line`);*/
+
+/*
+/////////////////////////////////////////////
+//LEC --> 018 {IF/ELSE statement}
+const age = 13;
+//const isOldEnough = age >= 18;
+
+if (age >= 18) {
+    console.log(`Sarah can start driving licence 🚗`);
+} else {
+    const yearsLeft = 18 - age;
+    //console.log(18 - age);
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);*/

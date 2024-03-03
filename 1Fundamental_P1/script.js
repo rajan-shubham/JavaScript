bf904077e6a88c84d4212fe8b7bf2c90a9b51e2f
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-03-03 07:16:39 +0530

    chalange of bmi by using if/else and backticks

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -190,3 +190,34 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);*/
+
+////////////////////////////////
+//LEC --> 19 
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+HINT: Use an if/else statement 😉
+
+GOOD LUCK 😀
+*/
+/*
+let marksWeight = 95; //kg
+let marksHeight = 1.88; //meter tall 
+let marksBmi = marksWeight / marksHeight ** 2;
+console.log(`Mark's BMI : ${marksBmi}`);
+
+let johnWeight = 85;
+let johnHeight = 1.76;
+let johnBmi = johnWeight / (johnHeight * johnHeight);
+console.log(`John's BMI : ${johnBmi}`);
+
+if (marksBmi > johnBmi) {
+    console.log(`Mark's BMI (${marksBmi}) is higher than John's (${johnBmi})!`);
+} else {
+    console.log(`Jons's BMI (${johnBmi}) is higher than Mark's (${marksBmi})!`);
+}*/

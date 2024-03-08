9dddd11a12a4af763bcf5ff6cd9c6c209f0ea158
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-03-08 20:37:10 +0530

    Boolean logic

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -318,3 +318,30 @@ if (age >= 20 || age < 30) {
 }
 
 console.log(!0);*/
+
+/////////////////////////////////////////////
+//LEC --> 024 {LOGICAL OPERATOR}
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log("Sarah is able to drive!");
+// } else {
+//     console.log("Someone else should drive...");
+// }
+
+const isTired = false;
+console.log(hasDriversLicense || hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}

e25ebaf01d226ac70bd6b1d79222f933fa7c5c26
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-03-12 01:24:06 +0530

    Ternary operator & Templet litrals(string) challenge

--- 1Fundamental_P1/script.js
+++ 1Fundamental_P1/script.js
@@ -477,7 +477,7 @@ console.log(`I'm ${2037 - 1991} years old ${me}`);*/
 //LEC --> 028 {CONDITIONAL(TERNARY) OPERATOR}
 
 //conditional operator --> allow us to write similar to an if else statement but all in one line
-
+/*
 const age = 23;
 age >= 18 ? console.log("I like to drink wine 🍷") : console.log("I like to drink water💧.")
 // operater always produces a value(as it is an expression) like here ternary operator.
@@ -493,4 +493,26 @@ if (age >= 18) {
 }
 console.log(drink2);
 
-console.log(`I like to drink ${age >= 18 ? 'wine🍷' : 'water💧'}.`);
+console.log(`I like to drink ${age >= 18 ? 'wine🍷' : 'water💧'}.`);*/
+
+////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+*/
+/*
+const bill = 400;
+let tip = bill >= 50 && bill <= 300 ? (bill * 0.15) : (bill * 0.2);
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);*/

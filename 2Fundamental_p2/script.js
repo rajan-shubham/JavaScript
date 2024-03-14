4e209c19f86c2dabfc14f416026d30d6b0620bb0
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-03-14 15:52:14 +0530

    Arrow function

--- 2Fundamental_p2/script.js
+++ 2Fundamental_p2/script.js
@@ -40,3 +40,46 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');*/
+
+//////////////////////////////////////////
+// LEC --> {034} (FUNCTION DECLERATION VS. EXPRESSION)
+/*
+//function decleration
+function calcAge1(birthYear) {
+    // const age = 2037 - birthYear;
+    // return age;
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+
+//function expression (as expression produce value, so we just assign value produced in variable calcAge2)
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+// in JS FUNCTION IS NOT A TYPE(like string number), BUT IT IS VALUE
+// DIFF. B/W fun. dec. & fun. exp.
+// practical diff. --> we can actually call fun. dec. before they are defined in the code
+*/
+/////////////////////////////////////////
+// LEC --> 035 {ARROW FUNCTIONS}
+/*
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));*/

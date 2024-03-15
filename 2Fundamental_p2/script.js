93005fcfaa42e00c0736311a8f78b53bac0f69c8
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-03-15 11:26:25 +0530

    function calling another function

--- 2Fundamental_p2/script.js
+++ 2Fundamental_p2/script.js
@@ -83,3 +83,44 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob'));*/
+
+/////////////////////////////////////////////
+// LEC --> 036 {FUN. CALLING OTHER FUN.}
+/*
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));*/
+/*
+// LEC --> 037; {REVIEWING FUN.}
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`)
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`)
+        return -1;
+    }
+
+    // return `${firstName} retires in ${retirement} years.`;
+};
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));*/

164d8b6c49db9d60ed56a07aff62859b0d11efca
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-04-22 14:31:09 +0530

    JS Object Methods

--- 2Fundamental_p2/script.js
+++ 2Fundamental_p2/script.js
@@ -377,3 +377,49 @@ if ( 14 >= 13) {
 }else{
     sonsol.log(`14 is smaller`);
 }
+
+///////////////////////////////////////////////
+// lec --> 044 {object Methods}
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    // calcAge: function (birthYear) {
+    //     return 2037 - birthYear;
+    // }
+
+    // calcAge: function () {
+    //     // console.log(this);
+    //     return 2037 - this.birthYear;
+    // }
+
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    getSummery: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+};
+
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// console.log(jonas.calcAge(1991));
+// console.log(jonas['calcAge'](1991));
+
+console.log(jonas.calcAge()); // only need to calculate age once
+
+console.log(jonas.age); // now we can simply retrieve the property that we already calculated before
+console.log(jonas.age);
+console.log(jonas.age);
+
+// challenge
+// "Jonas is a 46-year old teacher, and he has a/no driver's license" 
+console.log(jonas.getSummery()); // which is the object calling the method --> it is jonas , therefore the this keyword inside of getSummery will be jonas

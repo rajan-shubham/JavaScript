2abaab78ec629ecd32db78bcb78944fad4db846b
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-06-16 12:57:25 +0530

    destructuring object

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -116,3 +116,45 @@ const [p = 1, q = 1, r = 1] = [8, 9]; // when we don't know the actual size of a
 // (how many element an array have we don't know) this happens when we get data from API
 console.log(p, q, r);
 */
+/*
+///////////////////////////////////////
+// Destructuring Objects lec-->104
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/

926c7196d1a92cc7c2b23b46cbe631d800508d17
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-08-07 17:52:52 +0530

    function exceptiong ^ returning functions

--- 10Function/script.js
+++ 10Function/script.js
@@ -67,5 +67,93 @@ checkIn(flight, jonas);
 console.log(jonas);
 // JS has only passing by value not passing by reference
 */
-
+/*
 ////////////////////////////////////////
+// lec --> 128 {First-Class and Higher-Order Functions}
+// 1> First class functions 
+// Function are just another "type" of object
+// Function are simply vlues
+
+// store function in variables or properties:
+const add = (a, b) => a + b;
+const counter = {
+    value: 23,
+    inc: function () { this.value++; },
+}
+
+// pass functions as arguments to OTHER fnction:
+const greet = () => console.log("Hey, Shubham");
+// btnClose.addEventListener("click", greet)
+
+// Return functions FROM functions
+
+// call methods on functions:
+// counter.inc.bind(someOtherObject);
+
+// 2> Higher order functions 
+// a> A function that receive another function as an argument, that return an new function, or both 
+// ex -> higher order fun. (addEventListener), which take call back fun. (greet)
+// b> Function that returns new function
+function count() { // count() is higher order fun.
+    let counter = 0;
+    return function () { // returned fun.
+        counter++;
+    };
+}
+
+let cout = count();
+console.log(typeof(cout)); // function
+*/
+/*
+/////////////////////////////////////
+// lec --> 129 (Function Accepting callback functions)
+const oneWord = function (str) {
+    return str.replace(/ /g, "").toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(" ");
+    return [first.toUpperCase(), ...others].join(" ");
+}
+
+// Higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+// JS uses callbacks all the time
+// callbacks function helps alot it doing abstraction
+// abstraction : hiding implementation
+const high5 = function () {
+    console.log("👋");
+}
+document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5); 
+*/
+/*
+/////////////////////////////////
+// lec --> 130 {function Returning Functions}
+
+const greet = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    }
+}
+
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Shubham");
+
+greet("Hello")("shubham");
+
+// Challenge
+const arrowGreet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+const namaste = arrowGreet("Namaste");
+namaste("Rajan");
+*/

e62c30c7a4cbabb205d21ba6b68cef67467acfb7
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-08-08 08:53:16 +0530

    call and apply methods of functon

--- 10Function/script.js
+++ 10Function/script.js
@@ -157,3 +157,54 @@ const arrowGreet = (greeting) => (name) => console.log(`${greeting} ${name}`);
 const namaste = arrowGreet("Namaste");
 namaste("Rajan");
 */
+
+//////////////////////////////////////
+// lec--> 131 {The call and apply methods}
+const lufthansa = {
+    airline: "Lufthansa",
+    iataCode: "LH",
+    bookings: [],
+    // book: function() {}
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+    },
+};
+
+lufthansa.book(239, "Jonas Schmedtmann");
+lufthansa.book(635, "Shubham Rajan");
+console.log(lufthansa);
+
+const eurowings = {
+    airline: "Eurowings",
+    iataCode: 'EW',
+    bookings: [],
+}
+
+const book = lufthansa.book; // book function become normal function (so this method points to undefined when it is called as normaly/regularly)
+// Does NOT work
+// book(23, "Sarah William");
+
+// Call method
+book.call(eurowings, 23, "Sarah Williams")
+// remember a function is just an objet, & objects have methods & therefore function can be have methods to.
+console.log(eurowings);
+
+book.call(lufthansa, 239, "Marry Cooper")
+console.log(lufthansa);
+
+const swiss = {
+    airline: "Swiss Air Lines",
+    iataCode: "LX",
+    bookings: [],
+};
+
+book.call(swiss, 583, "Marry Cooper");
+console.log(swiss);
+
+// Apply method
+const flightData = [583, "George Cooper"];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData)

869eb4f8340c84374ead6cbbdc2218d4b4c28eff
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-09-27 13:55:40 +0530

    closure in JS

--- 10Function/script.js
+++ 10Function/script.js
@@ -365,6 +365,7 @@ console.log(notPrivate);
 */
 /*
 ///////////////////////////////////////
+// lec --> 135
 // Closures
 const secureBooking = function () {
   let passengerCount = 0;
@@ -385,6 +386,7 @@ console.dir(booker);
 
 
 ///////////////////////////////////////
+// lec --> 136
 // More Closure Examples
 // Example 1
 let f;
@@ -426,4 +428,29 @@ const boardPassengers = function (n, wait) {
 
 const perGroup = 1000;
 boardPassengers(180, 3);
+*/
+
+///////////////////////////////////////
+// lec --> 137
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
 */

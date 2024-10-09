a4b82d9cf30c4aece0c8c7f213b3e9a953c247de
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-10-09 12:49:31 +0530

    at method

--- 11Arrays-banklist/script.js
+++ 11Arrays-banklist/script.js
@@ -130,3 +130,18 @@ movements.forEach(function (move, i, arr) {
 });
 
 //NOTE: in forEach loop => we can't use break; continue; statement
+
+///////////////////////////////////////
+// lec --> 142 {at method}
+// The new at Method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));

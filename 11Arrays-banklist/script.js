bdce8a37b902598b2dda8ebe9f7dbd4d304e6959
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-16 16:15:52 +0530

    forEach method on Map & Set, at(0) operator

--- 11Arrays-banklist/script.js
+++ 11Arrays-banklist/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /*
 /////////////////////////////////////////////////
@@ -109,7 +103,7 @@ console.log(letters.join(" -> "));
 
 /////////////////////////////////////////
 // lec --> 141 Looping Arrays_ forEach
-
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // for (const movement of movements) {
 for (const [i, movement] of movements.entries()) {
@@ -145,3 +139,25 @@ console.log(arr.at(-1));
 
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
+
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+  ['INR', 'Indian National Rupee'],
+]);
+
+console.log(currencies);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR", "INR", "INR"]);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, key, set) {
+  console.log(`${key}: ${value}`); // here key and value are exactly the same
+});
+*/

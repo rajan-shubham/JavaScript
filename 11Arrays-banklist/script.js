01b72e1cbb1c69d17a2d11c141c2b249cfe44033
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-11-17 14:17:57 +0530

    container movements DOM manipulation

--- 11Arrays-banklist/script.js
+++ 11Arrays-banklist/script.js
@@ -61,6 +61,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (mov, i) {
+
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `;
+
+    /* https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML */
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+    
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
